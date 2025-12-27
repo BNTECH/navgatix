cd4a8962a62372e4a9f5995b860b94d6e78ba0c5
bntech <bntech@gmail.com>
2025-12-27 15:23:59 +0530

    #update users

--- satguruApp.Service/ViewModels/AuthenticationViewModel.cs
+++ satguruApp.Service/ViewModels/AuthenticationViewModel.cs
@@ -8,11 +8,11 @@ namespace satguruApp.Service.ViewModels
 {
     public class AuthenticationViewModel
     {
-        public string Message { get; set; }
+        public string? Message { get; set; }
         public bool IsAuthenticated { get; set; }
-        public string UserName { get; set; }
-        public string Email { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
         public List<string> Roles { get; set; }
-        public string Token { get; set; }
+        public string? Token { get; set; }
     }
 }
