e0fa093a1598e270813b4b4c15fd230e435dd73c
bntech <bntech@gmail.com>
2026-01-16 00:17:20 +0530

    #update country service

---
+++ satguruApp.Service/Services/CountryService.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using satguruApp.DLL.Models;
+using satguruApp.Service.Services.Interfaces;
+using satguruApp.Service.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace satguruApp.Service.Services
+{
+    public class CountryService : Repository<Country>, ICountryService
+    {
+        public CountryService(SatguruDBContext context) : base(context)
+        { }
+        private SatguruDBContext _db => (SatguruDBContext)_context;
+        public async Task<CountryViewModel> GetById(int? id, string name = "")
+        {
+            return await (from country in _db.Countries
+                          where country.Id == id.GetValueOrDefault() && country.Name.ToLower() == (string.IsNullOrEmpty(name) ? country.Name : name).ToLower() && country.IsDeleted == false
+                          select new CountryViewModel
+                          {
+                              Id = country.Id,
+                              Name = country.Name,
+                              CountryCodeTwo = country.CountryCodeTwo,
+                              CountryCodeThree = country.CountryCodeThree,
+                              RegionName = country.RegionName,
+                              IsDeleted = country.IsDeleted,
+                              CreatedBy = country.CreatedBy,
+                              CreatedDatetime = country.CreatedDatetime,
+                              UpdatedBy = country.UpdatedBy,
+                              UpdatedDatetime = country.UpdatedDatetime
+                          }).FirstOrDefaultAsync();
+        }
+        public async Task<CountryViewModel> SaveChangeAsync(CountryViewModel countryView)
+        {
+            var country = await (from contry in _db.Countries
+                                 where ((countryView.Id > 0 && contry.Id != countryView.Id) || (countryView.Id == 0) && (contry.Name.ToLower().Trim() == countryView.Name.ToLower().Trim()) && contry.IsDeleted == false)
+                                 select contry).FirstOrDefaultAsync();
+            if (country != null)
+            {
+                countryView.Message = "Country with same name already exists.";
+                return countryView;
+            }
+            else
+            {
+                country = await (from cntry in _db.Countries
+                                 where (countryView.Id > 0 && cntry.Id == countryView.Id && (cntry.Name.ToLower().Trim() == countryView.Name.ToLower().Trim()) && cntry.IsDeleted == false)
+                                 select cntry).FirstOrDefaultAsync();
+            }
+            if (country == null)
+            {
+                country = new Country();
+                countryView.ModelMapTo(country);
+                _db.Countries.Add(country);
+            }
+
+            var stsCount = await _db.SaveChangesAsync();
+            if (stsCount > 0)
+            {
+                countryView.Id = country.Id;
+
+            }
+            else
+            {
+                countryView.Message = "Error occurred while saving country details.";
+            }
+            return countryView;
+        }
+        public async Task<int> Delete(int id)
+        {
+            var countryDetail = await _db.Countries.Where(x => x.Id == id).FirstOrDefaultAsync();
+            countryDetail.IsDeleted = !countryDetail.IsDeleted;
+            return await _db.SaveChangesAsync();
+        }
+        public async Task<List<CountryViewModel>> GetAll(string name = "")
+        {
+            return await (from contry in _db.Countries
+                          where contry.IsDeleted == false && (string.IsNullOrEmpty(name) || contry.Name.ToLower().Contains(name.ToLower()))
+
+                          select new
+                          CountryViewModel
+                          {
+                              Id = contry.Id,
+                              Name = contry.Name,
+                              CountryCodeTwo = contry.CountryCodeTwo,
+                              CountryCodeThree = contry.CountryCodeThree,
+                              RegionName = contry.RegionName,
+                              IsDeleted = contry.IsDeleted,
+                              CreatedBy = contry.CreatedBy,
+                              CreatedDatetime = contry.CreatedDatetime,
+                              UpdatedBy = contry.UpdatedBy,
+                              UpdatedDatetime = contry.UpdatedDatetime
+                          }).ToListAsync();
+        }
+    }
+}
---
+++ satguruApp.Service/Services/StateService.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore;
+using satguruApp.DLL.Models;
+using satguruApp.Service.Services.Interfaces;
+using satguruApp.Service.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace satguruApp.Service.Services
+{
+    public class StateService : Repository<State>, IStateService
+    {
+        public StateService(SatguruDBContext context) : base(context)
+        { }
+        private SatguruDBContext _db => (SatguruDBContext)_context;
+        public async Task<StatelistViewModel> GetById(int? id, string name = "")
+        {
+            return await (from state in _db.States
+                          where state.Id == id.GetValueOrDefault() && state.Name.ToLower() == (string.IsNullOrEmpty(name) ? state.Name : name).ToLower() && state.IsDeleted == false
+                          select new StatelistViewModel
+                          {
+                              Id = state.Id,
+                              Name = state.Name,
+                              StateCode = state.StateCode,
+                              CountryId = state.CountryId,
+                              IsDeleted = state.IsDeleted,
+                              CreatedBy = state.CreatedBy,
+                              CreatedDatetime = state.CreatedDatetime,
+                              UpdatedBy = state.UpdatedBy,
+                              UpdatedDatetime = state.UpdatedDatetime
+                          }).FirstOrDefaultAsync();
+        }
+        public async Task<StatelistViewModel> SaveChangeAsync(StatelistViewModel stateView)
+        {
+            var states = await (from stat in _db.States
+                                where ((stateView.Id > 0 && stat.Id != stateView.Id) || stateView.Id == 0) && stat.CountryId == stateView.CountryId && (stat.Name.ToLower().Trim() == stateView.Name.ToLower().Trim() && stat.IsDeleted == false)
+                                select stat).FirstOrDefaultAsync();
+            if (states != null)
+            {
+                stateView.Message = "State with same name already exists.";
+                return stateView;
+            }
+            else
+            {
+                states = await (from stat in _db.States
+                                where (stateView.Id > 0 && stat.Id == stateView.Id && (stat.Name.ToLower().Trim() == stateView.Name.ToLower().Trim()) && stat.IsDeleted == false)
+                                select stat).FirstOrDefaultAsync();
+            }
+            if (states == null)
+            {
+                states = new State();
+                stateView.ModelMapTo(states);
+                _db.States.Add(states);
+            }
+
+            var stsCount = await _db.SaveChangesAsync();
+            if (stsCount > 0)
+            {
+                stateView.Id = states.Id;
+
+            }
+            else
+            {
+                stateView.Message = "Error occurred while saving state details.";
+            }
+            return stateView;
+        }
+        public async Task<int> Delete(int id)
+        {
+            var stateDetail = await _db.States.Where(x => x.Id == id).FirstOrDefaultAsync();
+            stateDetail.IsDeleted = !stateDetail.IsDeleted;
+            return await _db.SaveChangesAsync();
+        }
+        public async Task<List<StatelistViewModel>> GetAll(string name = "", int countryId = 0)
+        {
+            return await (from cust in _db.States
+                          where cust.IsDeleted == false && (string.IsNullOrEmpty(name) || cust.Name.ToLower().Contains(name.ToLower()) || (countryId > 0 && cust.CountryId == countryId))
+
+                          select new StatelistViewModel
+                          {
+                              Id = cust.Id,
+                              Name = cust.Name,
+                              StateCode = cust.StateCode,
+                              CreatedDatetime = cust.CreatedDatetime,
+                              CreatedBy = cust.CreatedBy,
+                              CountryId = cust.CountryId,
+                              UpdatedDatetime = cust.UpdatedDatetime,
+                              UpdatedBy = cust.UpdatedBy,
+                              IsDeleted = cust.IsDeleted,
+
+                          }).ToListAsync();
+        }
+    }
+}
